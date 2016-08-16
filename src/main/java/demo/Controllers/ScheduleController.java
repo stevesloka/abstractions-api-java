57e2eaeb1f2685bfd17c0e3a3c6b9d93a70f310b
Steve Sloka <steve@stevesloka.com>
2016-08-16 10:26:22 -0400

    DEMO

--- src/main/java/demo/Controllers/ScheduleController.java
+++ src/main/java/demo/Controllers/ScheduleController.java
@@ -1,6 +1,7 @@
 package demo.Controllers;
 
 import demo.AppVersion;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/version")
-public class VersionController {
+@RequestMapping("/sessions")
+public class ScheduleController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<? extends  Object> getAppVersion() {
-        return new ResponseEntity<>(new AppVersion("1.0.0"), HttpStatus.OK);
+        return new ResponseEntity<>(new ClassPathResource("data/sessions.json"), HttpStatus.OK);
     }
 }
