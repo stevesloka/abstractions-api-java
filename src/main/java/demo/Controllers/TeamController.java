a38274a86488df329318b15916b7c407866d0cd4
Steve Sloka <slokas@vmware.com>
2019-06-18 20:58:10 -0400

    Upgrade versions / Endpoints

--- src/main/java/demo/Controllers/TeamController.java
+++ src/main/java/demo/Controllers/TeamController.java
@@ -1,9 +1,10 @@
 package demo.Controllers;
 
-import demo.TeamRepository;
 import demo.TeamMember;
+import demo.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,8 @@ public class TeamController {
     @Autowired
     TeamRepository repository;
 
-    @RequestMapping("/team")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/team")
     public Iterable<TeamMember> team() {
         return repository.findAll();
     }
--- src/main/resources/db/migration/V2__Seed.sql
+++ src/main/resources/db/migration/V2__Seed.sql
@@ -3,7 +3,7 @@ INSERT INTO team_member (first_name, last_name) VALUES ('Thomas', 'Kraus');
 INSERT INTO team_member (first_name, last_name) VALUES ('Chuck', 'Bell');
 INSERT INTO team_member (first_name, last_name) VALUES ('Ashley', 'Barto');
 INSERT INTO team_member (first_name, last_name) VALUES ('Steve', 'Veverka');
-INSERT INTO team_member (first_name, last_name) VALUES ('Arial', 'Sanchez');
+INSERT INTO team_member (first_name, last_name) VALUES ('Ariel', 'Sanchez');
 INSERT INTO team_member (first_name, last_name) VALUES ('Mark', 'Harrison');
 INSERT INTO team_member (first_name, last_name) VALUES ('Yeshwant', 'Babar');
 INSERT INTO team_member (first_name, last_name) VALUES ('Randy', 'Snyder');
