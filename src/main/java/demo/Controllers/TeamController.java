33886de6367772a11c8393fe71a088690617bf22
Steve Sloka <slokas@vmware.com>
2019-06-19 02:00:09 +0000

    Move CORS annoatation

--- src/main/java/demo/Controllers/TeamController.java
+++ src/main/java/demo/Controllers/TeamController.java
@@ -4,17 +4,17 @@ import demo.TeamMember;
 import demo.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class TeamController {
 
     @Autowired
     TeamRepository repository;
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/team")
+    @RequestMapping("/team")
     public Iterable<TeamMember> team() {
         return repository.findAll();
     }
